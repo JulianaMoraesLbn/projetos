268924a29e817bd9380bed30e492e301372b5aaf
JulianaMoraesLbn <juliana.moraes@labenu.com.br>
2022-06-07 14:26:01 -0300

    alterações da correção do iago

--- sistema-bancario/src/index.ts
+++ sistema-bancario/src/index.ts
@@ -16,43 +16,98 @@ const anoAtual = dataAtual.getFullYear()
 const mesAtual = dataAtual.getMonth() + 1
 const diaAtual = dataAtual.getDay() + 1
 
-//visualizar todas as contas 
+
+const erros = {
+    CREDENCIAIS_INVALIDAS: { status: 401, message: "Verificar as credenciais" },
+    NAO_ENCONTRADO: { status: 404, message: "Não encontrado" },
+    CONTA_EXISTENTE: { status: 404, message: "Esta conta já existe" },
+    DADOS_AUSENTES: { status: 422, message: "Faltam informações, por favor, verificar." },
+    ERRO: { status: 500, message: "Falha. Algo deu errado" }
+}
+
+const sucesso = {
+    SUCESSO: { status: 200, message: "Realizado com sucesso!" },
+    CRIADO_SUCESSO: { status: 201, message: "Conta criado com sucesso!" },
+}
+
 app.get('/contas', (req: Request, res: Response) => {
+    try{
 
-    try {
-
-        res.status(successCode).send(contas)
-
-    } catch (error) {
-        res.status(errorCode).send(error)
+        if(!contas){
+            throw new Error(erros.ERRO.message)
+        }
+        const allContas = contas.map(conta => conta.nome)
+        res.status(sucesso.SUCESSO.status).send(allContas)
+    }
+    catch(error: any){
+        switch (error.message) {
+            case erros.CREDENCIAIS_INVALIDAS.message:
+                res.status(erros.CREDENCIAIS_INVALIDAS.status).send(erros.CREDENCIAIS_INVALIDAS.message)
+                break;
+            case erros.NAO_ENCONTRADO.message:
+                res.status(erros.NAO_ENCONTRADO.status).send(erros.NAO_ENCONTRADO.message)
+                break;
+            case erros.CONTA_EXISTENTE.message:
+                res.status(erros.CONTA_EXISTENTE.status).send(erros.CONTA_EXISTENTE.message)
+                break;
+            case erros.DADOS_AUSENTES.message:
+                res.status(erros.DADOS_AUSENTES.status).send(erros.DADOS_AUSENTES.message)
+                break;
+            default:
+                res.status(erros.ERRO.status).send(erros.ERRO.message)
+        }
     }
 })
 
-//verificar saldo passando cpf
 app.get('/contas/cliente', (req: Request, res: Response) => {
 
     try {
         const { cpf } = req.body
-        contas.forEach((cadaConta) => {
 
-            if (cadaConta.cpf === cpf) {
-                return res.status(successCode).send({ saldo: cadaConta.saldo })
-            }
-        })
+        if (!cpf) { throw new Error(erros.NAO_ENCONTRADO.message) }
 
+        const contaUser = contas.find(conta => conta.cpf === cpf)
+        
 
+        if (!contaUser) {
 
-    } catch (error) {
-        res.status(errorCode).send(error)
+            throw new Error(erros.NAO_ENCONTRADO.message)
+        }
+    
+
+        res.status(sucesso.SUCESSO.status).send({ saldo: contaUser.saldo })
+
+    } catch (error: any) {
+
+        switch (error.message) {
+            case erros.CREDENCIAIS_INVALIDAS.message:
+                res.status(erros.CREDENCIAIS_INVALIDAS.status).send(erros.CREDENCIAIS_INVALIDAS.message)
+                break;
+            case erros.NAO_ENCONTRADO.message:
+                res.status(erros.NAO_ENCONTRADO.status).send(erros.NAO_ENCONTRADO.message)
+                break;
+            case erros.CONTA_EXISTENTE.message:
+                res.status(erros.CONTA_EXISTENTE.status).send(erros.CONTA_EXISTENTE.message)
+                break;
+            case erros.DADOS_AUSENTES.message:
+                res.status(erros.DADOS_AUSENTES.status).send(erros.DADOS_AUSENTES.message)
+                break;
+            default:
+                res.status(erros.ERRO.status).send(erros.ERRO.message)
+        }
     }
 })
 
-//criar conta nova
-app.post('/contas/criar', (req: Request, res: Response) => {
+//criar conta nova -ok
+app.post('/contas', (req: Request, res: Response) => {
 
     try {
 
         const { nome, cpf, dataNascimento, saldo, extrato } = req.body
+
+        if (!nome || !cpf || !dataNascimento || !saldo || !extrato) {
+            throw new Error(erros.DADOS_AUSENTES.message)
+        }
 
         const novaConta: conta = {
             nome,
@@ -62,11 +117,28 @@ app.post('/contas/criar', (req: Request, res: Response) => {
             extrato
         }
 
-        contas.push(novaConta)
-        res.status(successCode).send('Cadastro concluido')
+        console.log(novaConta)
 
-    } catch (error) {
-        res.status(errorCode).send(error)
+        contas.push(novaConta)
+        res.status(sucesso.CRIADO_SUCESSO.status).send(sucesso.CRIADO_SUCESSO.message)
+
+    } catch (error: any) {
+        switch (error.message) {
+            case erros.CREDENCIAIS_INVALIDAS.message:
+                res.status(erros.CREDENCIAIS_INVALIDAS.status).send(erros.CREDENCIAIS_INVALIDAS.message)
+                break;
+            case erros.NAO_ENCONTRADO.message:
+                res.status(erros.NAO_ENCONTRADO.status).send(erros.NAO_ENCONTRADO.message)
+                break;
+            case erros.CONTA_EXISTENTE.message:
+                res.status(erros.CONTA_EXISTENTE.status).send(erros.CONTA_EXISTENTE.message)
+                break;
+            case erros.DADOS_AUSENTES.message:
+                res.status(erros.DADOS_AUSENTES.status).send(erros.DADOS_AUSENTES.message)
+                break;
+            default:
+                res.status(erros.ERRO.status).send(erros.ERRO.message)
+        }
     }
 
 })
@@ -78,6 +150,10 @@ app.post('/contas/deposito', (req: Request, res: Response) => {
 
         const { cpf, saldo } = req.body
 
+        if (!cpf || !saldo) {
+            throw new Error(erros.DADOS_AUSENTES.message)
+        }
+
         contas.forEach((cadaConta) => {
 
             if (cadaConta.cpf === cpf) {
@@ -87,46 +163,83 @@ app.post('/contas/deposito', (req: Request, res: Response) => {
         })
 
 
-    } catch (error) {
-        res.status(errorCode).send(error)
+    } catch (error: any) {
+        switch (error.message) {
+            case erros.CREDENCIAIS_INVALIDAS.message:
+                res.status(erros.CREDENCIAIS_INVALIDAS.status).send(erros.CREDENCIAIS_INVALIDAS.message)
+                break;
+            case erros.NAO_ENCONTRADO.message:
+                res.status(erros.NAO_ENCONTRADO.status).send(erros.NAO_ENCONTRADO.message)
+                break;
+            case erros.CONTA_EXISTENTE.message:
+                res.status(erros.CONTA_EXISTENTE.status).send(erros.CONTA_EXISTENTE.message)
+                break;
+            case erros.DADOS_AUSENTES.message:
+                res.status(erros.DADOS_AUSENTES.status).send(erros.DADOS_AUSENTES.message)
+                break;
+            default:
+                res.status(erros.ERRO.status).send(erros.ERRO.message)
+        }
     }
 })
 
 //realizar pagamento
-app.post('/contas/cliente/pagamentos', (req: Request, res: Response) => {
+app.post('/contas/cliente/pagamentos', (req: Request<{}, {}, { cpf: string, valor: number, data: any, descricao: string }>, res: Response) => {
     try {
 
         const { cpf, valor, data, descricao } = req.body
 
-        const novaData: string[] = data.split('/')
-        const dia: number = Number(novaData[0])
+        if (!cpf || !valor || !data || !descricao) {
+            throw new Error(erros.DADOS_AUSENTES.message)
+        }
+
+        const novaData = (data as string).split('/')
+        const dia = Number(novaData[0])
         const mes: number = Number(novaData[1])
         const ano: number = Number(novaData[2])
 
+        let newExtrato
         contas.forEach((cadaConta) => {
 
             if (cadaConta.cpf === cpf) {
                 if (cadaConta.saldo >= valor) {
                     if (ano === anoAtual && mes >= mesAtual && dia >= diaAtual || data === isNaN || data === undefined) {
                         cadaConta.saldo -= valor
-                        const newExtrato = {
+                        newExtrato = {
                             valor,
                             data,
                             descricao
                         }
                         cadaConta.extrato.push(newExtrato)
-                        return res.status(successCode).send({ message: 'Pagamento realizado com sucesso', newExtrato })
+                        return newExtrato
                     }
-                    return res.status(successCode).send({ message: 'Data de pagamento não autorizada' })
+                    throw new Error(erros.DADOS_AUSENTES.message) //data de pagamento não autorizada
                 }
-                return res.status(successCode).send({ message: 'Saldo insuficiente' })
+                throw new Error(erros.ERRO.message) //'Saldo insuficiente'
             }
-            return res.status(successCode).send({ message: 'Conta não localizada' })
+            throw new Error(erros.NAO_ENCONTRADO.message) //'Conta não localizada' 
         })
 
+        res.status(sucesso.SUCESSO.status).send(newExtrato)
 
-    } catch (error) {
-        res.status(errorCode).send(error)
+
+    } catch (error: any) {
+        switch (error.message) {
+            case erros.CREDENCIAIS_INVALIDAS.message:
+                res.status(erros.CREDENCIAIS_INVALIDAS.status).send(erros.CREDENCIAIS_INVALIDAS.message)
+                break;
+            case erros.NAO_ENCONTRADO.message:
+                res.status(erros.NAO_ENCONTRADO.status).send(erros.NAO_ENCONTRADO.message)
+                break;
+            case erros.CONTA_EXISTENTE.message:
+                res.status(erros.CONTA_EXISTENTE.status).send(erros.CONTA_EXISTENTE.message)
+                break;
+            case erros.DADOS_AUSENTES.message:
+                res.status(erros.DADOS_AUSENTES.status).send(erros.DADOS_AUSENTES.message)
+                break;
+            default:
+                res.status(erros.ERRO.status).send(erros.ERRO.message)
+        }
     }
 })
 
@@ -136,24 +249,47 @@ app.post('/contas/transferencia', (req: Request, res: Response) => {
     try {
 
         const { cpfDestinatario, nomeDestinatario, cpfRemetente, nomeRemetente, valor } = req.body
-        console.log('dentro do try')
+
+        if(!cpfDestinatario || !nomeDestinatario || !cpfRemetente || !nomeRemetente || !valor){
+            throw new Error(erros.DADOS_AUSENTES.message)
+        }
+        
+        let contaDestinatarioAlterada
         contas.forEach((cadaConta) => {
-            console.log('entrou')
+            
             if (cadaConta.cpf === cpfRemetente && cadaConta.saldo >= valor) {
-                console.log('1 forEach', cadaConta.cpf)
+                
                 contas.forEach((contaDestinatario) => {
                     if (contaDestinatario.cpf === cpfDestinatario) {
                         cadaConta.saldo -= valor
                         contaDestinatario.saldo += valor
 
-                        return res.status(successCode).send({ menssage: `Transferencia realizada com sucesso no valor de: ${valor} / Destinatário: ${nomeDestinatario} Remetente: ${nomeRemetente}` })
+                        contaDestinatarioAlterada = contaDestinatario.cpf
+                        return contaDestinatarioAlterada
                     }
                 })
             }
         })
 
-    } catch (error) {
-        res.status(errorCode).send(error)
+        res.status(sucesso.SUCESSO.status).send(contaDestinatarioAlterada)
+
+    } catch (error: any) {
+        switch (error.message) {
+            case erros.CREDENCIAIS_INVALIDAS.message:
+                res.status(erros.CREDENCIAIS_INVALIDAS.status).send(erros.CREDENCIAIS_INVALIDAS.message)
+                break;
+            case erros.NAO_ENCONTRADO.message:
+                res.status(erros.NAO_ENCONTRADO.status).send(erros.NAO_ENCONTRADO.message)
+                break;
+            case erros.CONTA_EXISTENTE.message:
+                res.status(erros.CONTA_EXISTENTE.status).send(erros.CONTA_EXISTENTE.message)
+                break;
+            case erros.DADOS_AUSENTES.message:
+                res.status(erros.DADOS_AUSENTES.status).send(erros.DADOS_AUSENTES.message)
+                break;
+            default:
+                res.status(erros.ERRO.status).send(erros.ERRO.message)
+        }
     }
 })
 
